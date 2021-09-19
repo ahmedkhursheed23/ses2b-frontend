e4be8a5f6e8aa59cbf2055fdc9687063773ad9a8
npudhota <nik@flimo.co>
2021-09-19 14:10:02 +1000

    Profile Page (ID Card)

--- pages/dashboard/profile.js
+++ pages/dashboard/profile.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import SidebarV2 from '../../components/dashComponents/SidebarV2'
 import { Typography } from '@material-ui/core'
+import Link from 'next/link'
+import Image from 'next/image'
+import profileCard from '/public/profilecardblank.svg'
+import styles from '../../components/Profile.module.css'
+
+
 
 export default function exams() {
     return (
         <div>
             <SidebarV2>
-                <Typography variant="h5">
-                    This will display the users profile
-                </Typography>
+                <div className={styles.container}>
+                  <Image src={profileCard} alt="user profile card" style="width:100%;"/>
+                  <div className={styles.centered}>12345678</div>
+                  <div className={styles.centeredfirstName}>John</div>
+                  <div className={styles.centeredlastName}>Smith</div>
+                </div>
             </SidebarV2>
         </div>
     )
